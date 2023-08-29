57fa07d8543d5d619e55b3264f9decb589a8a346
computerextra <81916339+computerextra@users.noreply.github.com>
2023-08-29 12:18:58 +0200

    Datenschutz

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Container, Image } from "react-bootstrap";
+import { Button, Container, Image, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle } from "react-bootstrap";
+import { useState } from "react"
+import DatenschutzModal from "./DatenschutzModal";
 
 function App() {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   return (
-    <Container>
-      <Image src="/images/logo.jpg" fluid />
-      <h1>Vielen Dank für Ihren Besuch</h1>
+    <Container fluid="sm" className="mx-auto text-center">
+      <Image src="/images/logo.jpg" fluid alt="Citron & Schmitz Logo" />
+      <h1 className="mt-3 mb-3">Vielen Dank für Ihren Besuch</h1>
       <p>Sie erreichen uns Telefonisch oder per Email unter:</p>
       <p>
-        Tel.: 0561 / 1077-77 Fax.: 0561 / 1077-70 Mail: info [AT] citron-schmitz
+        Tel.: 0561 / 1077-77 <br /> Fax.: 0561 / 1077-70 <br /> Mail: info [AT] citron-schmitz
         [PUNKT] de
       </p>
       <h2>Impressum</h2>
@@ -27,16 +34,34 @@ function App() {
         Genehmigung nach § 34c GewO durch die Stadt Kassel, Ordnungsamt. <br />
         USt-IdNr.: DE 224270508
       </p>
+      <h3>Datenschutz</h3>
       <p>
-        <small>&copy; 2023 - Citron & Schmitz Immobilien Service GmbH</small>
+        <span className="text-primary" style={{ cursor: "pointer" }} onClick={handleShow}><u>Datenschutzerklärung</u></span>
+      </p>
+      <p>
+        <small className="fw-semibold">&copy; 2023 - Citron & Schmitz Immobilien Service GmbH</small>
         <br />
-        <small>
+        <small className="text-secondary">
           Website erstellt durch{" "}
           <a href="https://computer-extra.de" target="_blank">
             Computer Extra, Ernst & Jacob KG
           </a>
         </small>
       </p>
+
+      <Modal show={show} onHide={handleClose} centered size="lg">
+        <ModalHeader closeButton>
+          <ModalTitle>Datenschutzerklärung</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <DatenschutzModal />
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variant="secondary" onClick={handleClose}>Schließen</Button>
+        </ModalFooter>
+      </Modal>
+
     </Container>
   );
 }
